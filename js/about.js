c6c9e298ecd90d9ec9ff4603243c037aab4873eb
hpjaj <hmlevine@gmail.com>
2015-02-25 11:29:56 -0700

    Added current work samples, and associated files.  Updated profile pic.

---
+++ js/about.js
@@ -0,0 +1,8 @@
+$( document ).ready(function(){
+  $('#harry-pic').hover(function() {
+    this.src = 'images/harry-linkedin-color-circle.png';
+  }, function() {
+    this.src = 'images/harry-linkedin-bw-circle.png';
+  });
+});
+
